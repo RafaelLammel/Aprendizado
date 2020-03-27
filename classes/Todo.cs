cb524e4e2f2ed81f5f8bfd3699592c99bdac88c1
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-03-27 10:56:13 -0300

    Estrutura inicial

---
+++ classes/Todo.cs
@@ -0,0 +1,13 @@
+using System;
+using TodoApp.Models;
+
+namespace TodoApp.Classes
+{
+    public class Todo
+    {
+        public Todo()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ models/TodoTask.cs
@@ -0,0 +1,10 @@
+namespace TodoApp.Models
+{
+    public class TodoTask
+    {
+        
+        public int Id { get; set; }
+        public string Texto { get; set; }
+
+    }
+}
