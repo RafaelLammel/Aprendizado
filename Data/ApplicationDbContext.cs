1a4af0033587ba54f44ea1162977d938bbc29ffb
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-04-02 15:19:27 -0300

    Criando migrations de Lista e Tarefa

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TodoAspNET.Models;
 
 namespace TodoAspNET.Data
 {
@@ -12,5 +13,9 @@ namespace TodoAspNET.Data
             : base(options)
         {
         }
+
+        public DbSet<Lista> Listas { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
     }
 }
