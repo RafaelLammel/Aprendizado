d93b2f0d92c7cdc03bcdd48d86bca987690a2ed1
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-04-09 13:39:43 -0300

    Exercício Livro

---
+++ ProjetoLivro/src/interfaces/Publicacao.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public interface Publicacao {
+
+    void abrir();
+
+    void fechar();
+
+    void folhear(int pagina);
+
+    void avancarPag();
+
+    void voltarPag();
+
+}
