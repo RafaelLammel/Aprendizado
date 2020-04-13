72a9416bef4db6054b55ecdfe7006401426bac74
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2020-04-13 11:59:39 -0300

    Exercício Youtube

---
+++ ProjetoYoutube/src/projetoyoutube/AcoesVideo.java
@@ -0,0 +1,7 @@
+package projetoyoutube;
+
+public interface AcoesVideo {
+    void play();
+    void pause();
+    void like();
+}
---
+++ ProjetoYoutube/src/projetoyoutube/ProjetoYoutube.java
@@ -0,0 +1,25 @@
+package projetoyoutube;
+
+public class ProjetoYoutube {
+
+    public static void main(String[] args) {
+        Video v[] = new Video[3];
+        v[0] = new Video("Aula 1 POO");
+        v[1] = new Video("Aula 2 PHP");
+        v[2] = new Video("Aula 3 Java");
+
+        Gafanhoto g[] = new Gafanhoto[2];
+        g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
+        g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
+
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]);
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+
+        vis[1] = new Visualizacao(g[0], v[1]);
+        vis[1].avaliar(87.0f);
+        System.out.println(vis[0].toString());
+    }
+
+}
